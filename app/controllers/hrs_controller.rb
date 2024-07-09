5e4b1648407093e979883997838c8ed43eb3fe2a
Reetesh Kumar Pandey <rpandey@gammastack.com>
2024-07-09 18:27:04 +0530

    authentication

--- app/controllers/hrs_controller.rb
+++ app/controllers/hrs_controller.rb
@@ -1,23 +1,31 @@
 class HrsController < ApplicationController
+    skip_before_action :authenticate_user!, only: :index
+
     def index
     end
+
     def show
         @hr = Hr.find(params[:id])
     end
+
     def new
         @hr = Hr.new
     end
+
     def create
         @hr = Hr.new(hr_params)
         if @hr.save
-        redirect_to @hr
+         session[:user_id]=@hr.id
+         redirect_to @hr
         else
-        render :new
+         render :new
         end
     end
+
     def edit
         @hr = Hr.find(params[:id])
     end
+
     def update
         @hr = Hr.find(params[:id])
         if @hr.update(hr_params)
@@ -26,14 +34,16 @@ class HrsController < ApplicationController
             render :edit
         end
     end
+
     def destroy
         @article = Hr.find(params[:id])
         @article.destroy
         redirect_to root_path, status: :see_other
     end
+
     private
     def hr_params
         params.require(:hr).permit(:name, :email, :status, :password, :phone_number)
     end
-    
+
 end
